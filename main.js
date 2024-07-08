648ebf5b2685eb22807e4e39bd19f8b9dfb4c38f
Replit user <>
2024-07-07 23:08:13 -0700

    adding map

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ function updateGradient(x, y) {
 }
 
 onmousemove = (e) => {
+  if (!matchMedia('(pointer:fine)').matches) return
   updateGradient(e.x, e.y)
   onscroll = (e2) => {
     updateGradient(e.x, e.y)
@@ -30,7 +31,6 @@ onload = () => {
   exppar.children("exs").css("display", "none")
   for (let par of exppar) {
     $(par).on("click", () => {
-      console.log("e")
       if ($(par).children("exs").css("display") == "none") {
         $(par).children("exs").css("display", "inline")
         $(par).children("exs").before("<p id='extra-space'> </p>")
@@ -40,4 +40,28 @@ onload = () => {
       }
     })
   }
+
+  const myGraph = ForceGraph3D()(document.getElementById("3d-graph"))
+    .graphData({
+      "nodes": [
+          {
+            "id": "id1",
+            "name": "name1",
+            "val": 1
+          },
+          {
+            "id": "id2",
+            "name": "name2",
+            "val": 10
+          }
+      ],
+      "links": [
+          {
+              "source": "id1",
+              "target": "id2"
+          }
+      ]
+  })
+  .width($("#content").innerWidth())
+  .backgroundColor($("body").css("--background"))
 }
