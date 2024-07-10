b4be206826fa12dc9690391adadf7193c4de2485
Replit user <>
2024-07-09 20:22:47 -0700

    added lines

--- main.js
+++ main.js
@@ -44,126 +44,144 @@ onload = () => {
     })
   }
 
-  const simulation = d3.forceSimulation(
-    [
-      {
-        "index": 0,
-        "x": 0,
-        "y": 0,
-        "z": 0,
-        "vx": 0,
-        "vy": 0,
-        "vz": 0,
-        "fx": 0,
-        "fy": 0,
-        "fz": 0
-      },
-      {
-        "index": 1,
-        "x": 0/0,
-        "y": 0/0,
-        "z": 0/0,
-        "vx": 1,
-        "vy": 0,
-        "vz": 0
-      },
-      {
-        "index": 2,
-        "x": 0/0,
-        "y": 0/0,
-        "z": 0/0,
-        "vx": 0,
-        "vy": 0,
-        "vz": 0
-      },
-      {
-        "index": 3,
-        "x": 0/0,
-        "y": 0/0,
-        "z": 0/0,
-        "vx": 0,
-        "vy": 0,
-        "vz": 0
-      },
-      {
-        "index": 4,
-        "x": 0/0,
-        "y": 0/0,
-        "z": 0/0,
-        "vx": 0,
-        "vy": 0,
-        "vz": 0
-      }
-    ], 3)
-    .force("charge", d3.forceManyBody())
-    .force("center", d3.forceCenter())
+  const node_ids = [
+    {"id": "website", "tags": ["web", "3d"]},
+    {"id": "trail", "tags": ["hackclub"]},
+    {"id": "blot", "tags": ["hackclub", "3d"]},
+    {"id": "quicklearn", "tags": ["math"]},
+    {"id": "lambda", "tags": ["math"]},
+    {"id": "controller-instrument", "tags": ["bodge", "music"]},
+    {"id": "hack-hour-clock", "tags": ["electronics", "hackclub"]},
+    {"id": "circuits", "tags": ["math"]},
+    {"id": "domino", "tags": ["bodge"]},
+    {"id": "planet-sim", "tags": ["bodge"]},
+    {"id": "xkcd-guessr", "tags": ["web", "bodge"]},
+    {"id": "desmos3d", "tags": ["3d", "math"]}
+  ]
 
-    let nodes = simulation.nodes()
-    let links = []
-    for (let i = 0; i < nodes.length; i++) {
-      for (let j = i+1; j < nodes.length; j++) {
-        links.push({"source": i, "target": j})
-      }
+  let nodes = [{
+    "id": node_ids[0].id,
+    "tags": node_ids[0].tags,
+    "x": 0,
+    "y": 0,
+    "z": 0,
+    "vx": 0,
+    "vy": 0,
+    "vz": 0,
+    "fx": 0,
+    "fy": 0,
+    "fz": 0
+  }]
+  for (var i of node_ids.slice(1)) {
+    nodes.push({
+      "id": i.id,
+      "tags": i.tags,
+      "x": 0/0,
+      "y": 0/0,
+      "z": 0/0,
+      "vx": 0,
+      "vy": 0,
+      "vz": 0
+    })
+  }
+  const simulation = d3.forceSimulation(nodes, 3)
+  .force("charge", d3.forceManyBody().strength(-5))
+  .force("center", d3.forceCenter());
+
+  nodes = simulation.nodes()
+  let links = []
+  for (let i = 0; i < nodes.length; i++) {
+    for (let j = i+1; j < nodes.length; j++) {
+      if (nodes[i].tags.some(r => nodes[j].tags.includes(r))) links.push({"source": i, "target": j})
     }
-    simulation.force("link", d3.forceLink(links))
-    simulation.on("tick", () => nodes = simulation.nodes())
+  }
+  simulation.force("link", d3.forceLink(links).distance(5))
+  simulation.on("tick", () => {
+    nodes = simulation.nodes()
+    links = simulation.force("link").links()
+  })
 
-    let camera = new THREE.PerspectiveCamera( 70, 1/1, 0.1, 100 );
-    camera.position.z = -40;
+  let camera = new THREE.PerspectiveCamera( 70, 1/1, 0.1, 100 );
+  camera.position.z = -40;
 
-    let scene = new THREE.Scene();
+  let scene = new THREE.Scene();
 
-    let boxes = []
-    for (var n of nodes) {
-      const geometry = new THREE.BoxGeometry();
-      const material = new THREE.MeshBasicMaterial();
+  let boxes = []
+  for (var n of nodes) {
+    var geometry = new THREE.BoxGeometry();
+    var material = new THREE.MeshBasicMaterial();
 
-      let mesh = new THREE.Mesh(geometry, material);
-      mesh.position.set(n.x, n.y, n.z)
-      scene.add(mesh);
-      boxes.push(mesh)
-    }
+    let mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(n.x, n.y, n.z)
+    scene.add(mesh);
+    boxes.push(mesh)
+  }
 
-    let renderer = new THREE.WebGLRenderer({antialias: true, canvas: $("#3d-graph").get(0)})
-    renderer.setClearColor("#00000", 0)
-    renderer.setPixelRatio(window.devicePixelRatio * 1/3);
-    renderer.setSize($("#3d-graph").parent().width(), $("#3d-graph").parent().width())
-    renderer.setAnimationLoop(animate);
+  var lines = []
+  for (var l of links) {
+    var geometry = new THREE.BufferGeometry().setFromPoints([new THREE.Vector3(l.source.x, l.source.y, l.source.z), new THREE.Vector3(l.target.x, l.target.y, l.target.z)])
+    var material = new THREE.LineBasicMaterial({color: 0x0000ff});
 
-    const controls = new OrbitControls( camera, renderer.domElement );
-    controls.autoRotate = true
-    controls.enablePan = false
-    controls.enableZoom = false
-    controls.maxDistance = 40
-    controls.minDistance = 40
-
-    var current = 0
-    function animate() {
-
-      for (var i = 0; i < boxes.length; i++) {
-        boxes[i].position.set(nodes[i].x, nodes[i].y, nodes[i].z)
-      }
-
-      boxes[0].rotation.x -= 0.005;
-      boxes[0].rotation.y -= 0.01;
-      controls.update()
-      renderer.render(scene, camera);
-
-    }
-
-    onkeydown = (e) => {
-      if (e.key != "n") return
-
-      current = (current + 1) % nodes.length
-      changeTargetTo(boxes[current], controls)
-    }
-}
-
-function changeTargetTo(box, controls) {
-  var counter = 0
-  var interval = setInterval(() => {
-    controls.target.lerp(box.position, (++counter)/10)
-    if (counter >= 10) clearInterval(interval)
-  }, 25)
+    let mesh = new THREE.Line(geometry, material);
+    scene.add(mesh)
+    lines.push(mesh)
+  }
   
+
+  let renderer = new THREE.WebGLRenderer({antialias: true, canvas: $("#3d-graph").get(0)})
+  renderer.setClearColor("#00000", 0)
+  renderer.setPixelRatio(window.devicePixelRatio * 1/3);
+  renderer.setSize($("#content").width(), $("#content").width())
+  renderer.setAnimationLoop(animate);
+
+  const controls = new OrbitControls( camera, renderer.domElement );
+  controls.autoRotate = true
+  controls.enablePan = false
+  controls.enableZoom = false
+  controls.maxDistance = 10
+  controls.minDistance = 10
+  controls.enableDamping = true
+
+  var current = 0
+
+  changeTargetTo(boxes[0])
+
+  function animate() {
+
+    var can_travel_to = []
+    for (var i = 0; i < boxes.length; i++) {
+      boxes[i].position.set(nodes[i].x, nodes[i].y, nodes[i].z)
+    }
+
+    for (var i = 0; i < lines.length; i++) {
+      var l = links[i]
+      lines[i].geometry.setFromPoints([new THREE.Vector3(l.source.x, l.source.y, l.source.z), new THREE.Vector3(l.target.x, l.target.y, l.target.z)])
+    }
+
+    boxes[0].rotation.x -= 0.005;
+    boxes[0].rotation.y -= 0.01;
+    controls.update()
+    renderer.render(scene, camera);
+
+  }
+
+  onkeydown = (e) => {
+    if (e.key != "n") return
+
+    current = (current + 1) % nodes.length
+    changeTargetTo(boxes[current], controls)
+  }
+
+  var can_travel_to = []
+  function changeTargetTo(box) {
+    can_travel_to = []
+    var counter = 0
+    var interval = setInterval(() => {
+      controls.target.lerp(box.position, (++counter)/10)
+      if (counter >= 10) clearInterval(interval)
+    }, 25)
+    for (var n of nodes) if (n != nodes[current] && n.tags.some(r => nodes[current].tags.includes(r))) can_travel_to.push(n)
+    console.log(can_travel_to)
+    console.log(changeTargetTo)
+}
 }
