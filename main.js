db842c6579794992dd1f7cf1d795f50ddf8d94d2
Replit user <>
2024-07-09 18:09:46 -0700

    more mapping

--- main.js
+++ main.js
@@ -59,39 +59,83 @@ onload = () => {
         "x": 0/0,
         "y": 0/0,
         "z": 0/0,
+        "vx": 1,
+        "vy": 0,
+        "vz": 0
+      },
+      {
+        "index": 2,
+        "x": 0/0,
+        "y": 0/0,
+        "z": 0/0,
+        "vx": 0,
+        "vy": 0,
+        "vz": 0
+      },
+      {
+        "index": 3,
+        "x": 0/0,
+        "y": 0/0,
+        "z": 0/0,
+        "vx": 0,
+        "vy": 0,
+        "vz": 0
+      },
+      {
+        "index": 4,
+        "x": 0/0,
+        "y": 0/0,
+        "z": 0/0,
         "vx": 0,
         "vy": 0,
         "vz": 0
       }
     ], 3)
-    
-    d3.forceLink([{"source": 0, "target": 1}])
+    .force("charge", d3.forceManyBody())
+    .force("center", d3.forceCenter())
+
     let nodes = simulation.nodes()
+    let links = []
+    for (let i = 0; i < nodes.length; i++) {
+      for (let j = i+1; j < nodes.length; j++) {
+        links.push({"source": i, "target": j})
+      }
+    }
+    simulation.force("link", d3.forceLink(links))
     simulation.on("tick", () => nodes = simulation.nodes())
 
-    let camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 100 );
-    camera.position.z = 2;
+    let camera = new THREE.PerspectiveCamera( 70, 1/1, 0.1, 100 );
+    camera.position.z = 40;
 
     let scene = new THREE.Scene();
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial();
 
-    let mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
+    let boxes = []
+    for (var n of nodes) {
+      const geometry = new THREE.BoxGeometry();
+      const material = new THREE.MeshBasicMaterial();
 
-    let renderer = new THREE.WebGLRenderer({canvas: $("#3d-graph").get()});
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    renderer.setAnimationLoop( animate );
+      let mesh = new THREE.Mesh(geometry, material);
+      mesh.position.set(n.x, n.y, n.z)
+      scene.add(mesh);
+      boxes.push(mesh)
+    }
 
+    let renderer = new THREE.WebGLRenderer({antialias: true, canvas: $("#3d-graph").get(0)})
+    renderer.setClearColor("#00000", 0)
+    renderer.setPixelRatio(window.devicePixelRatio * 1/3);
+    renderer.setSize($("#3d-graph").parent().width(), $("#3d-graph").parent().width())
+    renderer.setAnimationLoop(animate);
 
-    window.addEventListener( 'resize', onWindowResize );
-
-
+    var current = 0
     function animate() {
 
-      mesh.rotation.x += 0.005;
-      mesh.rotation.y += 0.01;
+      for (var i = 0; i < boxes.length; i++) {
+        boxes[i].position.set(nodes[i].x, nodes[i].y, nodes[i].z)
+      }
+
+      boxes[0].rotation.x += 0.005;
+      boxes[0].rotation.y += 0.01;
+      camera.position.set(boxes[current].position)
 
       renderer.render( scene, camera );
 
